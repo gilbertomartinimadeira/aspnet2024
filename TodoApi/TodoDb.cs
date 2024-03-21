8ffd73cf2d4533d32463361ed35bbcfdcced864d
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-03-21 20:32:18 -0300

    Added Todo Api

---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> Todos {get;set;}    
+}
